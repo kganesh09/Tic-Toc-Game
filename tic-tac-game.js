48cd81fd279e6653bdf3fe0e8a05e4e7d4a4972d
ganesh <ganesh1861997@gmail.com>
2024-05-27 17:21:17 +0530

    js-updated

--- tic-tac-game.js
+++ tic-tac-game.js
@@ -1,1 +1,14 @@
-var a=10;
+var player1=true;
+var player2=false;
+
+function buttonNo(player){
+    if(player1){
+        document.getElementById(player).innerText="X";
+        player1=false;
+        player2=true;
+    }else if(player2){
+        document.getElementById(player).innerText="O";
+        player1=true;
+        player2=false;
+    }
+}
