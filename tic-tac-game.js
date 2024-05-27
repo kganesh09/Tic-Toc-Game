2f1e8ea1bfccc42b9afa61d8489d0a19e47413ef
ganesh <ganesh1861997@gmail.com>
2024-05-27 17:33:39 +0530

    again js updated

--- tic-tac-game.js
+++ tic-tac-game.js
@@ -2,13 +2,19 @@ var player1=true;
 var player2=false;
 
 function buttonNo(player){
-    if(player1){
-        document.getElementById(player).innerText="X";
-        player1=false;
-        player2=true;
-    }else if(player2){
-        document.getElementById(player).innerText="O";
-        player1=true;
-        player2=false;
+    var cell = document.getElementById(player);
+    if(cell.innerText.trim()===""){
+        if(player1){
+            document.getElementById(player).innerText="X";
+            player1=false;
+            player2=true;
+        }else if(player2){
+            document.getElementById(player).innerText="O";
+            player1=true;
+            player2=false;
+        }
+    }else{
+        alert("space is already occupied")
     }
+   
 }
